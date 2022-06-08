c62a855d6d1be83e53c5027011006b76aa636c58
Benjamin Qin <benjamin.qin@gmail.com>
2022-06-08 20:16:53 +0900

    add components for customer and order

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerDetailComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
